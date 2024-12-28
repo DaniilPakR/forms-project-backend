e0800fa18abf296ce2ff41821751b7c54ce4d6ab
Daniil Pak <sanva970@gmail.com>
2024-12-29 00:17:30 +0200

    w

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
   password: "parolka322",
-  host: "localhost",
+  host: "51.20.3.194",
   port: 5432,
-  database: "forms_project"
+  database: "forms"
 });
 
 module.exports = pool;
