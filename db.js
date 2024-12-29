0aed08d2a33be317a545ca041ee72460dc892e1e
Daniil Pak <sanva970@gmail.com>
2024-12-29 10:03:49 +0200

    update

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "parolka322",
-  host: "localhost",
+  user: "sanv",
+  password: "Q40YZNSRkYFz9oyTALW1uNAGnoqzM2Sv",
+  host: "dpg-ctofstbqf0us73ans68g-a.frankfurt-postgres.render.com", // Use full host
   port: 5432,
-  database: "forms_project"
+  database: "forms_641o",
+  ssl: {
+    rejectUnauthorized: false, // Required for Render's managed PostgreSQL
+  },
 });
 
 module.exports = pool;
